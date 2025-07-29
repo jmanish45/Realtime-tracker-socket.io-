b85789603e71b36948c7044fb08a5b9c4ecf017d
Manish Jaiswal <jmanish2406@gmail.com>
2025-07-30 01:10:42 +0530

    project started

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const http = require('http');
+
+const socketIo = require('socket.io');
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+
+
+server.listen(3000);
---
+++ public/js/script.js
@@ -0,0 +1,2 @@
+const socket = io();
+console.log('Socket.io client connected');
